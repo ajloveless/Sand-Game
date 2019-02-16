0c229d3ad551f5afeb2dd47802139b12645e23cc
Zach Minot <43081846+ZMinot@users.noreply.github.com>
2019-02-15 23:13:25 -0500

    water kinda updated
    bug with movings through walls but on the right track

--- Java/Elements/Subtypes/Air.java
+++ Java/Elements/Subtypes/Air.java
@@ -1,23 +1,18 @@
 package Elements.Subtypes;
 
-import Elements.Types.AirClass;
 import Elements.Element;
 
-public class Air extends AirClass
-{
- public Air()
- {
-  //Id, Name, Tooltip, Type, Color, Density
-  super((byte)0,"Air","Does nothing",(byte)0,0x000000,0);
+public class Air extends Element {
+    public Air() {
+        //Id, Name, Tooltip, Type, Color, Density
+        super((byte) 0, "Air", "Does nothing", (byte) 0, 0x000000, 0);
 
+    }
 
- }
+    @Override
+    public byte[] interaction(Element[] elements, byte[] grid, int i, int width) {
+        grid = super.interaction(elements, grid, i, width);
+        return grid;
+    }
 
- @Override
- public byte[] interaction(Element[] elements, byte[] grid, int i, int width)
- {
-  grid = super.interaction(elements, grid, i, width);
-  return grid;
- }
- 
 }
--- Java/Elements/Types/Liquid.java
+++ Java/Elements/Types/Liquid.java
@@ -2,50 +2,45 @@ package Elements.Types;
 
 import Elements.Element;
 
-public class Liquid extends Element
-{
+public class Liquid extends Element {
 
-    public Liquid(byte id, String name, String tooltip, byte type, int color, double density)
-    {
+    public Liquid(byte id, String name, String tooltip, byte type, int color, double density) {
         super(id, name, tooltip, type, color, density);
     }
 
-    public byte[] interaction(Element[] elements, byte[] grid, int i, int width)
-    {
+    public byte[] interaction(Element[] elements, byte[] grid, int i, int width) {
         grid = super.interaction(elements, grid, i, width);
-            int left = i - 1;
-            int right = i + 1;
-            int up = i - width;
-            int down = i + width;
-            int offset = (int) (Math.random()*3) - 1;
+        int left = i - 1;
+        int right = i + 1;
+        int up = i - width;
+        int down = i + width;
+        int offset = (int) (Math.random() * 3) - 1;
 
-            if(elements[grid[i]].density > elements[grid[down + offset]].density)
-            {
-            byte swap = grid[down + offset];
-            grid[down + offset] = grid[i];
-            grid[i] = swap;
-            } else if(elements[grid[i]].density > elements[grid[down - offset]].density)
-            {
-            byte swap = grid[down - offset];
-            grid[down - offset] = grid[i];
-            grid[i] = swap;
+        if (elements[grid[i]].density > elements[grid[down + offset]].density) {
+            grid = Element.swap(grid, i, down + offset);
+        } else if ((elements[grid[i]].density < elements[grid[down]].density)) {
+            int rightSpace = 0;
+            int leftSpace = 0;
+            for (int j = 0; j < 3; j++) {
+                if (elements[grid[i]].density > elements[grid[left - j]].density) leftSpace++;
+                else break;
             }
-            else if (elements[grid[i]].density > elements[grid[left]].density && elements[grid[i]].density > elements[grid[right]].density) {
-                byte swap = grid[i + offset];
-                grid[i + offset] = grid[i];
-                grid[i] = swap;
-            }
-            else if (elements[grid[i]].density > elements[grid[left]].density) {
-                        byte swap = grid[left];
-                        grid[left] = grid[i];
-                        grid[i] = swap;
-                    }
-            else if (elements[grid[i]].density > elements[grid[right]].density) {
-                        byte swap = grid[right];
-                        grid[right] = grid[i];
-                        grid[i] = swap;
 
-                    }
+            for (int j = 0; j < 3; j++) {
+                if (elements[grid[i]].density > elements[grid[right + j]].density) rightSpace++;
+                else break;
+            }
+
+            if (rightSpace > leftSpace) {
+                grid = Element.swap(grid, i, right + rightSpace);
+            } else if (leftSpace > rightSpace){
+                grid = Element.swap(grid, i, left - leftSpace);
+            }
+            else grid = Element.swap(grid, i, i + offset);
+
+        } else if ((elements[grid[i]].density == elements[grid[down + offset]].density)) {
+            grid = Element.swap(grid, i, i + offset);
+        }
 
         return grid;
     }
